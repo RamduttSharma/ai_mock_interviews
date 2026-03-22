5f3b92e8557a90ea7b79d9794a538569f9ba5f13
Ramdutt Sharma <ramusharma15236@gmail.com>
2026-03-23 00:15:13 +0530

    show username

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
-import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
+import { getCurrentUser, isAuthenticated, signOut } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
+  const user = await getCurrentUser()
   const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) redirect("/sign-in");
 
@@ -16,10 +17,15 @@ const Layout = async ({ children }: { children: ReactNode }) => {
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">PrepWise</h2>
         </Link>
-        <button style={{cursor:"pointer", display:"flex" }} onClick={signOut}>
-          <img src="/image.png" alt='signOut' width={"37px"}/>
-          <p style={{height:"50%", marginTop:"5px"}}>signOut</p>
-        </button>
+        <div className="flex flex-row justify-between">
+          <h3>Welcome, {user?.name} &nbsp;</h3> <h2>|</h2>
+          <form action={signOut}>
+            <button style={{ cursor: "pointer", display: "flex", marginLeft: "15px" }}>
+              <img src="/image.png" alt="signOut" width="37px" />
+              <p style={{ height: "50%", marginTop: "5px" }}>signOut</p>
+            </button>
+          </form>
+        </div>
       </nav>
 
       {children}
